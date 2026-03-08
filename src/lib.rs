a346df5f7a5cb19587b32f3a656096ad2e1b1aef
Christopher Ariza <ariza@flexatone.com>
2026-03-08 15:42:36 -0700

    1.3.0 RC 1

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,5 @@ mod render;
 mod score;
 
 pub use color::{score_to_hue, HueResult};
-pub use parse::{detect_cvss_version, is_version3, parse_cvss, CvssVersion, ParsedMetrics};
 pub use render::render_glyph;
 pub use score::calculate_score;
